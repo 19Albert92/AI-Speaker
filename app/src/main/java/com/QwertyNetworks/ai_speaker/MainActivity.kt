4b451c9818a96728cc8e52b3935e9021eeb9610b
19Albert92 <Albert.zinnurov2017@yandex.ru>
2022-01-28 14:17:05 +0100

    Initial commit

---
+++ app/src/main/java/com/QwertyNetworks/ai_speaker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.QwertyNetworks.ai_speaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.QwertyNetworks.ai_speaker.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/QwertyNetworks/ai_speaker/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.QwertyNetworks.ai_speaker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
