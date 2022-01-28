d2878e3078f3ca7ad6aa88cd5fd6869974a58cf1
19Albert92 <Albert.zinnurov2017@yandex.ru>
2022-01-28 18:30:49 +0100

    create main_webview

--- app/src/main/java/com/QwertyNetworks/ai_speaker/ui/main/MainFragment.kt
+++ app/src/main/java/com/QwertyNetworks/ai_speaker/ui/main/MainFragment.kt
@@ -1,13 +1,17 @@
 package com.QwertyNetworks.ai_speaker.ui.main
 
+
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.QwertyNetworks.ai_speaker.R
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.QwertyNetworks.ai_speaker.databinding.MainFragmentBinding
+import com.QwertyNetworks.ai_speaker.ui.Constance
 
 class MainFragment : Fragment() {
 
@@ -25,17 +29,49 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = MainFragmentBinding.inflate(inflater, container, false)
+        initialWebView()
         return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+
+    }
+
+    private fun initialWebView() {
+        binding!!.mainWebView.apply {
+            settings.javaScriptEnabled = true
+            settings.allowContentAccess = true
+            settings.allowFileAccess = true
+            settings.userAgentString = Constance.USER_AGENT
+            settings.javaScriptCanOpenWindowsAutomatically = true
+
+            loadUrl(Constance.LOAD_URL)
+
+            val newWebViewClient: WebViewClient = object : WebViewClient() {
+
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String) : Boolean {
+                    view?.loadUrl(Constance.LOAD_URL)
+                    return true
+                }
+
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    view?.loadUrl(request?.url.toString())
+                    return true
+                }
+
+            }
+            webViewClient = newWebViewClient
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
-
 }
